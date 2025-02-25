b2b4abc6df1c867b57fa7a855032f6438a6b593c
SunshineAlis <bumaam@yahoo.com>
2025-02-25 11:31:34 +0800

    1

---
+++ src/controllers/user/luser.login.js
@@ -0,0 +1,24 @@
+// const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcrypt");
+// const User = require("../models/User");
+
+// exports.login = async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+//     const user = await User.findOne({ email });
+
+//     if (!user) {
+//       return res.status(404).json({ message: "User not found" });
+//     }
+
+//     const isMatch = await bcrypt.compare(password, user.password);
+//     if (!isMatch) {
+//       return res.status(401).json({ message: "Invalid credentials" });
+//     }
+
+//     const token = jwt.sign({ id: user._id }, "your_secret_key", { expiresIn: "1h" });
+//     res.json({ token });
+//   } catch (error) {
+//     res.status(500).json({ message: "Server error" });
+//   }
+// };
