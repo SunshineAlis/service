18a8d115bb2ad7805fdb6c290ee653e7e2565490
SunshineAlis <bumaam@yahoo.com>
2025-03-20 17:54:04 +0800

    1

--- src/model/foodOrder.model.js
+++ src/model/foodOrder.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const foodOrderSchema = new mongoose.Schema({
   user: { type: mongoose.Types.ObjectId, require: true, ref: "users" },
   totalPrice: { type: Number, require: true },
-  foodOrderItam: { type: mongoose.Types.ObjectId, require: true, ref: "foodOrderItem" },
+  foodOrderItem: { type: mongoose.Types.ObjectId, require: true, ref: "foodOrderItem" },
   status: { type: String, enum: ["PENDING", "CANCELED", "DELIVERED"] },
   image: { type: String, require: true },
   category: { type: mongoose.Types.ObjectId, ref: "categories" },
--- src/routers/order.router.js
+++ src/routers/order.router.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { createOrder } from "../controllers/foodOrder/foodOrder.post.js";
-import { updateOrder } from "../controllers/foodOrder/foodOrder.put.js"
-import { deleteOrder } from "../controllers/foodOrder/foodOrder.delete.js"
+import { updateOrder } from "../controllers/foodOrder/foodOrder.put.js";
+import { deleteOrder } from "../controllers/foodOrder/foodOrder.delete.js";
+import { getOrders } from "../controllers/foodOrder/foodOrder.get.js";
 const orderRouter = express.Router();
 
 orderRouter.post("", createOrder);
 orderRouter.put("/:orderId", updateOrder);
 orderRouter.delete("", deleteOrder);
+orderRouter.get("", getOrders);
 
 export default orderRouter;
